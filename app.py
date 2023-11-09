011623703f64f872e12a3aaf16d081bd8e80ba25
WilderVilleda <148466596+WilderVilleda@users.noreply.github.com>
2023-11-08 22:12:31 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
-import dash
-from dash import dcc
-from dash import html
-from dash import dash_table
-from dash.dependencies import Input, Output
-import plotly.express as px
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.by import By
+from datetime import datetime, timedelta
 import pandas as pd
-import dash_bootstrap_components as dbc
+import numpy as np
+import requests
+import plotly as pl
+import plotly.express as px
+from dash import Dash, dcc, html, dash_table, Input, Output
+import dash
+from dash import dash_table
 
-df= pd.read_csv("imm01.csv")
+df= pd.read_excel("unificado.xlsx")
+df
 
 #construir dashboard
 app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
@@ -15,7 +21,7 @@ server=app.server
 
 app.title="Dashboard"
 
-cuentas=["Inflacion"]
+cuentas=["Inflacion","Remesas", "Emision"]
 
 #layout del app
 app.layout = html.Div([
@@ -23,14 +29,14 @@ app.layout = html.Div([
         
     #primer drop down para elegir las empresas
     html.Div(dcc.Dropdown(
-    id="empresasdd",value=["2015","2016","2017","2018","2019","2020","2021","2022","2023"],clearable=False, multi=True,
-    options=[{'value':x,'value':x} for x in sorted(df.Año.unique())]
+    id="inflaciondd",value=["2015","2016","2017","2018","2019","2020","2021","2022","2023"],clearable=False, multi=True,
+    options=[{'label':x,'value':x} for x in sorted(df.Año.unique())]
     ),className="six columns", style={"width":"50%"},),
     
     #revisar
     html.Div(dcc.Dropdown(
-    id="cuentadd",value="Inflacion",clearable=False,
-    options=[{'value':x,'value':x} for x in cuentas]
+    id="indicador_indd",value="Inflacion",clearable=False,
+    options=[{'label':x,'value':x} for x in cuentas]
     ), className="six columns"), 
     ], className="row"),],className="custom-dropdown"),
     
@@ -41,6 +47,8 @@ app.layout = html.Div([
                                                     }),],style={'width':'1100px'}),
     html.Div([dcc.Graph(id="boxplot",figure={},)],style={"width":'1100px'}),
     
+    html.Div([dcc.Graph(id="pie",figure={},)],style={"width":'1100px'}),
+    
     #tabla
     html.Div(html.Div(id="table-container"),style={'marginBottom':'15px','marginTop':
                                                  "10px"}),])
@@ -50,9 +58,10 @@ app.layout = html.Div([
 @app.callback(
     [Output(component_id="graph",component_property="figure"),
     Output(component_id="boxplot",component_property="figure"),
+    Output(component_id="pie",component_property="figure"),
     Output("table-container",'children')],
-    [Input(component_id="empresasdd",component_property="value"),
-    Input(component_id="cuentadd",component_property="value")]
+    [Input(component_id="inflaciondd",component_property="value"),
+    Input(component_id="indicador_indd",component_property="value")]
 )
 
 #definicion de la funcion
@@ -62,9 +71,10 @@ def display_value(selected_company,selected_account):
         df2=df[df["Año"].isin(["2015","2016","2017","2018","2019","2020","2021","2022","2023"])]
     else:
         df2=df[df["Año"].isin(selected_company)]
+        
     
     #grafica1
-    fig= px.line(df2,color="Año",x="Periodo",markers=True,y=selected_account,
+    fig= px.line(df2,color="Año",x="Mes",markers=True,y=selected_account,
                 width=1000,height=500)
     
     fig.update_layout(title=f'{selected_account} de {selected_company}',
@@ -77,12 +87,16 @@ def display_value(selected_company,selected_account):
     fig2.update_layout(title=f'{selected_account} de {selected_company}',
                       )
     
+    #grafica 3
+    fig3= px.pie(df2, names="Año", values=selected_account, width=1000, height=500)
+    fig3.update_traces(title=f'Representación anual de la {selected_account} total del periodo {selected_company}')
+    
     #modificar data frame para poder hacerlo tabla
-    df_reshaped = df2.pivot(index='Año', columns='Periodo', values=selected_account)
+    df_reshaped = df2.pivot(index='Año', columns='Mes', values=selected_account)
     df_reshaped_2 = df_reshaped.reset_index()
 
     #tabla
-    return (fig,fig2,
+    return (fig,fig2,fig3,
            dash_table.DataTable(columns=[{"name":i,"id":i} for i in df_reshaped_2],
                                data=df_reshaped_2.to_dict("records"),
                                export_format="csv",#para guardar como csv
@@ -90,6 +104,7 @@ def display_value(selected_company,selected_account):
                                style_header={'backgroundColor':'blue',
                                             'color':'white'},
                                ))
-#setear server y correr
-if __name__=='__main__':
+#setear server y correr:
+
+if __name__ == '__main__':
     app.run_server(debug=False,host="0.0.0.0",port=10000)
